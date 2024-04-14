5df294eeffbaab8be447e76e6583b941eeefe5c1
Rishab Jain <Rishabjain@ucla.edu>
2024-04-14 16:11:50 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/Homescreen";
 
